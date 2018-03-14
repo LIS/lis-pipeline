007dc37aaa110bc134fb22df3c38949cd5aee45d
Adrian Vladu <avladu@cloudbasesolutions.com>
2018-03-14 14:59:22 +0200

    [reporting] Do an update instead of insert for unique run

--- scripts/reporting/parser.py
+++ scripts/reporting/parser.py
@@ -86,19 +86,75 @@ def insert_values(cursor, values_dict):
         sys.exit(0)
 
 
+def update_values(cursor, values_dict, composite_keys):
+    """Creates an update command from a template and calls the pyodbc method.
+
+     Provided with a dictionary that is structured so the keys match the
+     column names and the values are represented by the items that are to be
+     inserted the function composes the sql command from a template and
+     calls a pyodbc to execute the command.
+    """
+    update_command_template = Template(
+        """
+        IF (NOT EXISTS(SELECT * FROM $tableName WHERE $compositeConditions))
+        BEGIN
+            INSERT INTO $tableName($columns) VALUES($insertValues)
+        END
+        ELSE
+        BEGIN
+            UPDATE $tableName SET $updateValues WHERE $compositeConditions
+        END
+    """
+    )
+    logger.debug('Line to be update %s', values_dict)
+    insert_values = ''
+    update_values = ''
+    composite_conditions = ''
+    table_name = '"' + env.str('TableName') + '"'
+    for k, v in values_dict.items():
+        insert_values = ', '.join([str(insert_values), "'" + str(v) + "'"])
+        update_values = ', '.join([str(update_values),
+                                  str(k) + " = " + "'" + str(v) + "'"])
+        if k in composite_keys:
+            composite_conditions = (' AND '.join([str(composite_conditions),
+                                    str(k) + " = " + "'" + str(v) + "'"]))
+
+    update_command = update_command_template.substitute(
+        tableName=table_name,
+        columns=', '.join(values_dict.keys()),
+        insertValues=insert_values[1:],
+        updateValues=update_values[1:],
+        compositeConditions=composite_conditions[5:]
+        )
+
+    logger.debug('Update command that will be exectued:')
+    logger.debug(update_command)
+
+    try:
+        cursor.execute(update_command)
+    except pyodbc.DataError as data_error:
+        print(dir(data_error))
+        if data_error[0] == '22001':
+            logger.error('Value to be updated exceeds column size limit')
+        else:
+            logger.error('Database update error', exc_info=True)
+
+        logger.debug('Terminating execution')
+        sys.exit(0)
+
+
 def main():
     env.read_envfile('db.config')
     logger.debug('Initializing database connection')
     db_connection, db_cursor = init_connection()
 
-    logger.debug('Executing insertion commands')
     data = json.load(open('tests.json'))
 
     for row in data:
         print(row)
-        insert_values(db_cursor, row)
+        update_values(db_cursor, row, ('PipelineName', 'PipelineBuildNumber'))
 
-    logger.debug('Executing insertion commands')
+    logger.debug('Executing db commands')
     db_connection.commit()
 
 
