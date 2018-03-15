fd719828b387d66a9a593b613ecbdab16d43632c
mihaico <mcostache@cloudbasesolutions.com>
2018-03-15 16:20:09 +0200

    [lis-rpm] Add local hyper-v perf trigger

--- scripts/lis_hyperv_platform/trigger_perf_tests.ps1
+++ scripts/lis_hyperv_platform/trigger_perf_tests.ps1
@@ -1,25 +1,39 @@
 param(
-    [parameter(Mandatory=$true)]
-    [String] $KernelVersionPath,
-    [parameter(Mandatory=$true)]
-    [String] $LocalJenkinsPerfURL,
-    [parameter(Mandatory=$true)]
-    [String] $LocalJenkinsPerfToken
+    [Parameter(ParameterSetName='lis',Mandatory=$false)][string]$LisUrl,
+    [Parameter(ParameterSetName='lis',Mandatory=$true)][string]$AzureToken,
+    [Parameter(ParameterSetName='lis',Mandatory=$true)][string]$FullPerfRun,
+    [Parameter(ParameterSetName='msft',Mandatory=$false)][string]$KernelVersionPath,
+    [parameter(Mandatory=$true)][String] $LocalJenkinsPerfURL,
+    [parameter(Mandatory=$true)][String] $LocalJenkinsPerfToken
 )
 
 $ErrorActionPreference = "Stop"
 
-$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
-Import-Module "$scriptPath\ini.psm1"
+function Trigger-LisPerfRun {
+    Write-Host "Triggering performance jobs for LIS"
+    Invoke-RestMethod -Method Post `
+        -Uri "${LocalJenkinsPerfURL}/buildWithParameters?LIS_URL=${LisUrl}&AZURE_TOKEN=${AzureToken}&`
+        FULL_PERF_RUN=${FullPerfRun}&token=${LocalJenkinsPerfToken}"
+}
 
-function Main {
+function Trigger-MsftPerfRun {
+    Write-Host "Triggering performance jobs for kernel folder: ${KernelVersionPath}"
+    $scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+    Import-Module "$scriptPath\ini.psm1"
     Write-Host $KernelVersionPath
     Write-Host (cat $KernelVersionPath)
-    $kernelPath = Get-IniFileValue -Path $KernelVersionPath `
-        -Section "KERNEL_BUILT" -Key "folder"
-    Write-Host "Triggering performance jobs for kernel folder: ${kernelPath}"
+    $kernelPath = Get-IniFileValue -Path $KernelVersionPath -Section "KERNEL_BUILT" -Key "folder"
     Invoke-RestMethod -Method Post `
         -Uri "${LocalJenkinsPerfURL}/buildWithParameters?KERNEL=${kernelPath}&token=${LocalJenkinsPerfToken}"
+}
+
+function Main {
+    if ($LisUrl) {
+        Trigger-LisPerfRun
+    }
+    elseif ($KernelVersionPath) {
+        Trigger-MsftPerfRun
+    }
 }
 
 Main
