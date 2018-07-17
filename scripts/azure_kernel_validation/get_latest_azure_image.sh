a8385521cb945c12f7ba275e07314475d09d0dee
Adrian Vladu <avladu@cloudbasesolutions.com>
2018-07-17 13:51:31 +0300

    azure_latest_image: Added pipeline and watcher

---
+++ scripts/azure_kernel_validation/get_latest_azure_image.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+LATEST_IMAGES_PATH=""./latest_images.txt""
+get_latest_image() {
+    local publisher=$1
+    local sku=$2
+    last_image=$(az vm image list --all --location "westus2" \
+      --publisher "${publisher}" --sku "${sku}" | grep -v "SAP" \
+      | grep -v "CI" | grep -v "DAILY" | grep "urn" \
+      | sort -r --version-sort | sed -n '1 p' | awk '{print $2}')
+    last_image=${last_image//\"/}
+    last_image=${last_image//:/ }
+    last_image=${last_image//,/}
+    echo "Last image for publisher ${publisher} and sku ${sku}: ${last_image}"
+    echo "${last_image};" >> "${LATEST_IMAGES_PATH}"
+}
+get_latest_image $@
+
