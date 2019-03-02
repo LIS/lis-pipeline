508f36c891a52ec4772221cbda67ec899eb3fa6a
Johnson <v-jogeor@microsoft.com>
2019-03-01 16:55:19 -0800

    rhel_validation: Handle kernel install failure and version check enhancement
    Modification to handle the errata kernel installation failure and
    appropiately displaying inbox driver when LIS module is not installed.

--- scripts/rhel_validation/prepare_lis_vm.sh
+++ scripts/rhel_validation/prepare_lis_vm.sh
@@ -21,20 +21,34 @@ function prepare_vm {
     kernel_version="$3"
     rhel_username="$4"
     rhel_password="$5"
-    
+
     subscription-manager register --force --username "${rhel_username}" --password "${rhel_password}"
     subscription-manager attach --auto
     subscription-manager release --set=${os_version}
     subscription-manager repos --enable=${kernel_repo}
     yum clean all
-    yum -y install wget gcc
+    yum -y install wget gcc redhat-lsb-core
     yum -y install kernel-${kernel_version}
+    if [[ $? -ne 0 ]];then
+	    printf "\n kernel-${kernel_version} installation failed \n"
+    fi
     yum -y install kernel-devel-${kernel_version}
 }
 
 function install_modules {
     modules_path="$1"
-    
+    kernel_version="$2"
+
+    #Check whether ERRATA kernel is installed or not
+    installed_kernel_version=$(uname -r)
+    required_kernel_version=$kernel_version.$(uname -m)
+
+    if [[ $installed_kernel_version != $required_kernel_version ]];then
+            printf "\nERRATA kernel $kernel_version not installed \n"
+            printf "\nAborting LIS installation \n"
+            return 0
+    fi
+
     pushd "${modules_path}"
     update_folder="$(ls -1v | grep update | tail -1)"
     pushd "./${update_folder}"
@@ -48,9 +62,16 @@ function install_modules {
     printf "\nKMOD Install: \n"
     printf "\nKMOD Install: \n" 1>&2
     rpm -ivh "$(ls kmod* | grep -vE 'i686')"
+    if [[ $? -ne 0 ]];then
+	    printf "\n$(ls kmod* | grep -vE 'i686') installation failed \n"
+    fi
     printf "\nLIS Install: \n"
     printf "\nLIS Install: \n" 1>&2
     rpm -ivh "$(ls microsoft* | grep -vE 'src|debug|i686')"
+    if [[ $? -ne 0 ]];then
+	    #Not critical for aborting. Print message for information.
+	    printf "\n$(ls microsoft* | grep -vE 'src|debug|i686') installation failed \n"
+    fi
     
     popd
     popd
@@ -76,10 +97,10 @@ function main {
         else 
             KERNEL_REPO="rhel-${MAJOR_VERSION}-server-eus-rpms"
         fi
-        prepare_vm "$OS_VERSION" "$KERNEL_REPO" "$KERNEL_VERSION" "$RHEL_USERNAME" "$RHEL_PASSWORD" 
+        prepare_vm "$OS_VERSION" "$KERNEL_REPO" "$KERNEL_VERSION" "$RHEL_USERNAME" "$RHEL_PASSWORD"
     elif [[ "$SECTION" == "install_lis" ]];then
         MODULES_DIR="$(get_lis_os RHEL "${OS_VERSION}")"
-        install_modules "${LIS_PATH}/${MODULES_DIR}"
+        install_modules "${LIS_PATH}/${MODULES_DIR}" "$KERNEL_VERSION"
     fi
     popd
 }
