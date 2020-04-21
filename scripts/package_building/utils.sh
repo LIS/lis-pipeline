6c26be2f2943e3a25fffc3696d0ecc327bd4fa6d
Lili Deng <lildeng@microsoft.com>
2020-04-20 23:49:52 -0700

    Build 114

--- scripts/package_building/utils.sh
+++ scripts/package_building/utils.sh
@@ -253,7 +253,7 @@ get_stable_branches() {
     git_dir="$1"
 
     pushd "$git_dir"
-    branches=$(git branch -r | sort | grep "origin/linux-msft-4.19.112")
+    branches=$(git branch -r | sort | grep "origin/linux-msft-4.19.114")
     for branch in $branches;do
         small_branch="${branch/origin\//}"
         tag=$(get_git_tag "." $branch)
